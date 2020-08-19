4a8e92a5ff67898aaa5312e30bae174d0d86ebc6
Oscar Freij <otg020313@gmail.com>
2020-08-19 20:01:50 +0200

    Dump

---
+++ Assets/Resources/Scripts/MouseFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = transform.position.z - Camera.main.transform.position.z;
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+    }
+}
